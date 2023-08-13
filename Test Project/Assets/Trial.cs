7f630b496dadfbcc2d643bbd0e1bb0fe2b2be606
Tanish-Raorane <tanishraorane4@gmail.com>
2023-08-14 01:37:15 +0530

    .

---
+++ Test Project/Assets/Trial.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trial : MonoBehaviour
+{
+    private PlaceIndicator placeIndicator;
+    public GameObject ObjectToPlace;
+    MeshRenderer mr;
+    //bool isSpawned = false;
+
+    void Start()
+    {
+        placeIndicator = FindObjectOfType<PlaceIndicator>();
+        mr = ObjectToPlace.GetComponent<MeshRenderer>();
+        mr.enabled = false;
+    }
+
+    public void PressToPlace()
+    {
+
+
+        mr.enabled = true;
+            //ObjectToPlace.transform.position = placeIndicator.transform.position;
+        
+        //if (SpawnedObject == null)
+        //{
+        //    if (placeIndicator.Indicator.activeInHierarchy)
+        //    {
+        //        SpawnedObject = Instantiate(ObjectToPlace, placeIndicator.transform.position, ObjectToPlace.transform.rotation);
+        //    }
+
+        //}
+    }
+}
