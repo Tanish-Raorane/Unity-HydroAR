e3e2237b7d91372e41db178f5f8ca078f4c766d5
Tanish-Raorane <tanishraorane4@gmail.com>
2023-08-20 14:27:04 +0530

    .

--- Test Project/Assets/Scripts/Scale_Rotate.cs
+++ Test Project/Assets/Scripts/Scale_Rotate.cs
@@ -13,6 +13,10 @@ public class Scale_Rotate : MonoBehaviour
     GameObject Pivot_Point;
     public Slider scale_slider;
     public Slider rotate_slider;
+    public GameObject pivot;
+    Quaternion rotation;
+    Vector3 scale;
+    bool First_Time = true;
     
 
     void Start()
@@ -20,27 +24,44 @@ public class Scale_Rotate : MonoBehaviour
         ARSO_Holder holder = FindObjectOfType<ARSO_Holder>();
         ARSO = holder.arSessionOrigin;
         Ind = GameObject.FindWithTag("PlaceIndicator");
-        Pivot_Point = GameObject.FindWithTag("PivotPoint");
+        //Pivot_Point = GameObject.FindWithTag("PivotPoint");
         gameObject.transform.position = Ind.transform.position;
-        Pivot_Point.transform.position = gameObject.transform.position;
+        //Pivot_Point.transform.position = gameObject.transform.position;
+        //pivot.transform.position = gameObject.transform.position;
+        //ARSO.MakeContentAppearAt(pivot.transform, pivot.transform.position, pivot.transform.rotation);
         //ARSO.MakeContentAppearAt(Pivot_Point.transform, Pivot_Point.transform.position, Pivot_Point.transform.rotation);
         button = GameObject.FindWithTag("PlaceModelButton");
         button.SetActive(false);
         Ind.SetActive(false);
+        scale = ARSO.transform.localScale;
+        rotation = ARSO.transform.rotation;
     }
 
     public void Scale()
     {
-        ARSO.MakeContentAppearAt(Pivot_Point.transform, Pivot_Point.transform.position, Pivot_Point.transform.rotation);
+        //ARSO.MakeContentAppearAt(Pivot_Point.transform, Pivot_Point.transform.position, Pivot_Point.transform.rotation);
+        ARSO.MakeContentAppearAt(pivot.transform, pivot.transform.position, pivot.transform.rotation);
+        //ARSO.MakeContentAppearAt(gameObject.transform, gameObject.transform.position, gameObject.transform.rotation);
         float val = scale_slider.value;
+        ARSO.transform.rotation = rotation;
+        if (First_Time)
+        {
+            ARSO.transform.localScale = scale;
+            First_Time = false;
+        }
         ARSO.transform.localScale = new Vector3(val, val, val);
+        
+        
     }
 
     public void Rotate()
     {
-        ARSO.MakeContentAppearAt(Pivot_Point.transform, Pivot_Point.transform.position, Pivot_Point.transform.rotation);
+        //ARSO.MakeContentAppearAt(Pivot_Point.transform, Pivot_Point.transform.position, Pivot_Point.transform.rotation);
+        ARSO.MakeContentAppearAt(pivot.transform, pivot.transform.position, pivot.transform.rotation);
+        //ARSO.MakeContentAppearAt(gameObject.transform, gameObject.transform.position, gameObject.transform.rotation);
         float val = rotate_slider.value;
         ARSO.transform.rotation = Quaternion.Euler(0, val, 0);
+        rotation = ARSO.transform.rotation; 
     }
 
     //private void Update()
